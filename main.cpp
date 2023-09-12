7f4855cb86100bff785197765c41df48c9a275a2
vyahislav <khv-rvv@mail.ru>
2023-09-13 00:28:09 +1000

    first commit

---
+++ main.cpp
@@ -0,0 +1,26 @@
+﻿#include<iostream>
+using namespace std;
+
+struct point
+{
+	int m_x;
+	int m_y;
+};
+
+void print_point(const point& point_object)
+{
+	std::cout << "x: " << point_object.m_x << ", y: " << point_object.m_y << std::endl;
+}
+
+int main()
+{
+	point my_point;
+
+	for (int i = 0; i < 5; i++)
+	{
+		my_point.m_x = i;
+		my_point.m_y = i * 2;
+		print_point(my_point);
+	}
+	return 0;
+}
